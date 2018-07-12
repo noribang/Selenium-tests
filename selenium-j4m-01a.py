29eb80e2b3507fe0adff9fc9d08d5b8001910b65
noribang <hoonk9387@gmail.com>
2018-07-12 13:34:46 -0500

    Changed code block below while loop to get url of content page at every iteraton of loop and clear content of input element.

--- selenium-j4m-01a.py
+++ selenium-j4m-01a.py
@@ -10,20 +10,8 @@ from selenium.webdriver.common.keys import Keys
 ## driver = webdriver.Firefox()
 #b. This will open Chrome browser.
 driver = webdriver.Chrome()
-# Webdriver instance will navigate to URL that has
-# been passed to get(). 
-driver.get("http://www.jokes4miles.com/jokescontent?category")
-# Check if string provided is in title element.
-assert "Jokes4Miles - Content" in driver.title
-# Find the element by id attribute.
-elem = driver.find_element_by_id("content-search")
-# Print element found to shell.
-print("elem is: " + str(elem))
-# Clear contents of element.
-# e.g. Text from input element.
-elem.clear()
 # List of strings to test for content.
-contentList = ["tina fey", "pat tomasulo"]
+contentList = ["tina fey", "pat tomasulo", "bill murray"]
 # Loop through list to check for content.
 # for i in contentList:
 
@@ -31,22 +19,31 @@ contentList = ["tina fey", "pat tomasulo"]
 i = 0
 # Loop through list.
 while i < len(contentList):
-	print(contentList[i]) 
-	# Send string passed into method to element found.
-	elem.send_keys(contentList[i])
-	# Programmatically enter Return key by calling send_key().
-	elem.send_keys(Keys.RETURN)
-	# Wait 4 seconds.
-	sleep(4)
-	# Check if string provided is NOT IN the html of the page.
-	assert "No results found." not in driver.page_source
+    # Webdriver instance will navigate to URL that has been passed to get(). 
+    driver.get("http://www.jokes4miles.com/jokescontent?category")
+    # Check if string provided is in title element.
+    assert "Jokes4Miles - Content" in driver.title
+    # Find the element by id attribute.
+    elem = driver.find_element_by_id("content-search")
+    # Print element found to shell.
+    print("elem is: " + str(elem))
 	# Clear input.
-	elem.clear()
+    elem.clear()
+    # Print list item to shell.
+    print(contentList[i]) 
+	# Send string passed into method to element found.
+    elem.send_keys(contentList[i])
+	# Programmatically enter Return key by calling send_key().
+    elem.send_keys(Keys.RETURN)
+	# Wait 4 seconds.
+    sleep(4)
+	# Check if string provided is NOT IN the html of the page.
+    assert "No results found." not in driver.page_source
 	# Increment counter.
-	i += 1
+    i += 1
 
 # Wait 5 seconds.
-sleep(8)
+sleep(5)
 # close() closes one tab.
 ## driver.close()
 # quit() closes entire browser.
